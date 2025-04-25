862f612ecbf8b3ea34f3973ff5128c23b98585d5
ryokoberryz <34004842+ryokoberryz@users.noreply.github.com>
2025-04-25 21:18:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,13 +13,10 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
-https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list_raw.txt
-https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt
-https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2
-https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/airport_sub_merge.txt
-https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/sub_merge.txt
-https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub
+https://api.xmancdn.net/osubscribe.php?sid=106223&token=6j6f5tNJFqcl
+https://sub2.smallstrawberry.com/api/v1/client/subscribe?token=f25c35b768f3e2ffb39d9c746ded563c
+https://rgergergergerg6555.saojc.xyz/api/v1/client/subscribe?token=bf548e010d97bba5b729bac471cf26b7
+https://flztjc.com/link/SdS0HTOePiNjphoS?trojan=1
 `
 
 
