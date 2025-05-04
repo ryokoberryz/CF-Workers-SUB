d31108c2c94bbb29958a0b3d0575250771984ac4
ryokoberryz <34004842+ryokoberryz@users.noreply.github.com>
2025-05-04 13:43:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -22,7 +22,9 @@ https://flztjc.com/link/SdS0HTOePiNjphoS?trojan=1
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/ryokoberryz/clash/refs/heads/main/qichiyu.ini"; //订阅配置文件
+//let subConfig = "https://raw.githubusercontent.com/ryokoberryz/clash/refs/heads/main/qichiyu.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/Aethersailor/Custom_Clash_Rules/refs/heads/main/cfg/Custom_Clash_Full.ini"; //订阅配置文件
+
 let subProtocol = 'https';
 
 export default {
